fd4fb236f552328e6289a35afd12ba1e5f5b90ca
Nahdasyifa <101632340+Nahdasyifa@users.noreply.github.com>
2022-03-23 22:01:46 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            // penentuan nilai properties
+            taxi.DriverName = "Sulis";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 25;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DroppOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
